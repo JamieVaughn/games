296f53ffe02b11831e15289c32ac6316cb52a492
JamieVaughn <wxv6@case.edu>
2020-12-13 20:13:43 -0500

    Build to docs

---
+++ docs/chunks/index.580b3e6b.js
@@ -0,0 +1,5 @@
+import{s,m as e}from"../index.b136099c.js";s("/assets/style.module.7b16dcc3.css");const t="about_migxty";export default({query:s})=>e`<section class=${t}>
+    <h1>About This Page</h1>
+    <p>A page all about this website.</p>
+    <pre>${JSON.stringify(s)}</pre>
+  </section>`;
